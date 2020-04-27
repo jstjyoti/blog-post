d1fd4ff4d133e7b7e6985337cb5b89d73bffd96f
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-04-27 14:40:19 +0530

    global axios interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Axios from 'axios';
+
+//potentially manipulating the data, commonly adding header etc...global axios
+var myInterceptor = Axios.interceptors.request.use(req =>
+  {
+    console.log(req);
+    //edit request config, for task you want to do globally for requests
+    return req;
+  }, error =>{
+    return Promise.reject(error);
+    //handled locally in catch of component
+  }
+);
+
+Axios.interceptors.response.use(res =>
+  {
+    console.log(res);
+    //edit request config, for task you want to do globally for requests
+    return res;
+  }, error =>{
+    return Promise.reject(error);
+    //handled locally in catch of component
+  }
+);
+//removing an interceptor
+Axios.interceptors.request.eject(myInterceptor);
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
